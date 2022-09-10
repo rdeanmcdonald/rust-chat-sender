1e7cfeb6860d21fba8972e615990edb513733dba
Richard McDonald <rmcdonald@wisesystems.com>
2022-09-10 14:35:59 -0400

    working post to kafka topic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use axum::http::StatusCode;
+// use axum::response::Result;
 use axum::{response::Json, routing::post, Router};
 use futures::future::join_all;
 use rdkafka::config::ClientConfig;
@@ -5,7 +7,7 @@ use rdkafka::error::KafkaError;
 use rdkafka::message::OwnedMessage;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::{json, Result, Value};
 use std::boxed::Box;
 use std::net::SocketAddr;
 use std::time::Duration;
@@ -34,8 +36,15 @@ async fn main() {
 
     // ########## SERVER ##########
     let server_handle = spawn(async move {
+        // kafka producer is cheap to clone
+        let producer: FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", "localhost:29092")
+            .set("message.timeout.ms", "5000")
+            .create()
+            .expect("Failed to create producer");
+
         // build our application with some routes
-        let app = Router::new().route("/send", post(send));
+        let app = Router::new().route("/send", post(move |body| send(body, producer.clone())));
 
         // run it with hyper
         let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -50,49 +59,75 @@ async fn main() {
     // let kafka_producer_handle = ...
     // follow instructions for this example:
     // https://github.com/fede1024/rust-rdkafka/blob/master/examples/simple_producer.rs
-    let producer_handle = spawn(async move {
-        let producer: FutureProducer = ClientConfig::new()
-            .set("bootstrap.servers", "localhost:29092")
-            .set("message.timeout.ms", "5000")
-            .create()
-            .expect("Failed to create producer");
+    // let producer_handle = spawn(async move {
+    //     let producer: FutureProducer = ClientConfig::new()
+    //         .set("bootstrap.servers", "localhost:29092")
+    //         .set("message.timeout.ms", "5000")
+    //         .create()
+    //         .expect("Failed to create producer");
 
-        let _result = produce(&producer).await;
-    });
+    //     let _result = produce(&producer).await;
+    // });
 
-    let handles = vec![server_handle, producer_handle];
+    // let handles = vec![server_handle, producer_handle];
+    let handles = vec![server_handle];
     let _results = join_all(handles).await;
 }
 
-async fn send(Json(client_message): Json<Value>) -> Json<Value> {
+async fn send(
+    Json(client_message): Json<Value>,
+    producer: FutureProducer,
+) -> axum::response::Result<Json<Value>, StatusCode> {
     tracing::debug!("Received ClientMessage {}", client_message);
-    Json(json!({ "data": 42 }))
+
+    match serde_json::from_value::<ClientMessage>(client_message) {
+        Ok(cm) => {
+            println!("HI");
+            let delivery_status = producer
+                .send(
+                    FutureRecord::to("topic_testing")
+                        .payload(&cm.content)
+                        .key("My key"),
+                    Duration::from_secs(0),
+                )
+                .await;
+
+            // This will be executed when the result is received.
+            tracing::debug!("Delivery status for message {:?} received", delivery_status);
+
+            return Ok(Json(json!({ "data": 42 })));
+        }
+        Err(e) => {
+            eprintln!("BOO");
+            return Ok(Json(json!({ "error": 42 })));
+        }
+    }
 }
 
-async fn produce(producer: &FutureProducer) -> Result<(), Box<dyn std::error::Error>> {
-    let _delivery_handles: Vec<JoinHandle<Result<(i32, i64), (KafkaError, OwnedMessage)>>> = (0..5)
-        .map(|i| {
-            // docs say it's cheap clone
-            let p = producer.clone();
-            spawn(async move {
-                let value = &format!("My message {}", i);
-                tracing::debug!("Preparing to produce record: {} {}", "alice", value);
+// async fn produce(producer: &FutureProducer) -> Result<(), Box<dyn std::error::Error>> {
+//     let _delivery_handles: Vec<JoinHandle<Result<(i32, i64), (KafkaError, OwnedMessage)>>> = (0..5)
+//         .map(|i| {
+//             // docs say it's cheap clone
+//             let p = producer.clone();
+//             spawn(async move {
+//                 let value = &format!("My message {}", i);
+//                 tracing::debug!("Preparing to produce record: {} {}", "alice", value);
 
-                let delivery_status = p
-                    .send(
-                        FutureRecord::to("topic_testing")
-                            .payload(value)
-                            .key(&format!("My key {}", i)),
-                        Duration::from_secs(0),
-                    )
-                    .await;
+//                 let delivery_status = p
+//                     .send(
+//                         FutureRecord::to("topic_testing")
+//                             .payload(value)
+//                             .key(&format!("My key {}", i)),
+//                         Duration::from_secs(0),
+//                     )
+//                     .await;
 
-                // This will be executed when the result is received.
-                tracing::info!("Delivery status for message {} received", i);
-                delivery_status
-            })
-        })
-        .collect();
+//                 // This will be executed when the result is received.
+//                 tracing::info!("Delivery status for message {} received", i);
+//                 delivery_status
+//             })
+//         })
+//         .collect();
 
-    Ok(())
-}
+//     Ok(())
+// }
